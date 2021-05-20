d80815977cd835f5098d8eae5122da415f9ff5a0
Belen Guijarro <belen@belen.com>
2021-05-20 20:59:00 +0200

    tercer commit creacion de una clase nueva

---
+++ src/BelenGuijarro.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Belén Guijarro Cubero
+ * @date 20-may-2021
+ * @time 20:52:49
+ * @encoding UTF-8
+ * @name BelenGuijarro
+ *
+ */
+public class BelenGuijarro {
+
+    private String examen = "Despliegues";
+    private String fecha = "Mayo 2021";
+    private String alumna = "Belén Guijarro";
+}
